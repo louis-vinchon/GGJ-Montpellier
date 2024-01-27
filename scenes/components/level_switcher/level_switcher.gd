2ffc7bc20aa90940c4935054672c87302d1f4894
Louis VINCHON <louis.vinchon.dev@gmail.com>
2024-01-27 11:35:18 +0100

    Add level switch functionality
    Needs some debugging

--- scenes/components/level_switcher/level_switcher.gd
+++ scenes/components/level_switcher/level_switcher.gd
@@ -4,6 +4,7 @@ class_name LevelSwitcher
 extends Area2D
 
 @export var next_level :Types.Level
+@export var next_level_spawn :String
 
 func _get_configuration_warnings():
 	var warnings :PackedStringArray = []
@@ -12,7 +13,11 @@ func _get_configuration_warnings():
 		warnings.append("A LevelSwitcher needs to be configured with a level (other than None).")
 	
 	return warnings
+	
+func _ready():
+	if !next_level_spawn:
+		next_level_spawn = Types.DEFAULT_SPAWN
 
 func _on_body_entered(body):
 	#if body is Player:
-	SignalBus.switch_level.emit(next_level)
+	SignalBus.switch_level.emit(next_level, next_level_spawn)
--- scripts/autoloads/signal_bus.gd
+++ scripts/autoloads/signal_bus.gd
@@ -2,4 +2,4 @@ extends Object
 
 # This autoloaded script is meant to be a central hub for signal sharing.
 
-signal switch_level(level: Types.Level)
+signal switch_level(level: Types.Level, spawn: String)
