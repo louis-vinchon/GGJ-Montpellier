3af77931835173a405d3a9e8080607e2115210ba
Louis VINCHON <louis.vinchon.dev@gmail.com>
2024-01-27 09:45:00 +0100

    Add autoloads

---
+++ scripts/autoloads/signal_bus.gd
@@ -0,0 +1,5 @@
+extends Object
+
+# This autoloaded script is meant to be a central hub for signal sharing.
+
+signal switch_level(level: Types.Level)
---
+++ scripts/autoloads/types.gd
@@ -0,0 +1,9 @@
+extends Object
+
+# This autoloaded script is meant to contain shared type definitions like enums.
+
+enum Level {
+	None, # "Null" value, when level is defined as 'Types.None' it will evaluate to false in if statements.
+	City,
+	Shop1,
+}
