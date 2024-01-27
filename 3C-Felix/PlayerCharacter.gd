52e3b19d88ed0a668159c79a8ab9b56cfd909ebf
FelixChevrel <59053658+FelixChevrel@users.noreply.github.com>
2024-01-27 11:28:48 +0100

    Update PlayerCharacter.gd
    Car base movement
    
    -metrics to adjust

--- 3C-Felix/PlayerCharacter.gd
+++ 3C-Felix/PlayerCharacter.gd
@@ -1,8 +1,10 @@
 extends RigidBody2D
 
-@export var accelerationPower : float = 1000
+@export var accelerationPower : float = 3000
+@export var rotationSpeed : float = 1000
 
 var acceleration : Vector2 = Vector2(0,0)
+var RotationIncreased : float = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,28 +15,34 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	var up = 0
-	var down = 0
-	var left = 0
-	var right = 0
+	var forwardInput = 0
 	
 	if (Input.is_action_pressed("UP")):
-		up = 1
+		forwardInput += 1
 	if (Input.is_action_pressed("DOWN")):
-		down = 1
+		forwardInput -= 1
+	
+	var rotateInput = 0
+	
 	if (Input.is_action_pressed("LEFT")):
-		left = 1
+		
+		rotateInput -= 1
+		pass
 	if (Input.is_action_pressed("RIGHT")):
-		right = 1
+		rotateInput += 1
+		pass
 	
+	RotationIncreased = delta * rotateInput * rotationSpeed
 	
-	acceleration = Vector2(right - left, down - up) * accelerationPower
+	acceleration = transform.x * forwardInput * accelerationPower * delta
 	
 	
 	pass
 
 
 func _physics_process(delta):
+	
+	apply_torque(RotationIncreased)
 	
 	apply_central_force(acceleration)
 	
